e2a41cdf27d89b9bc1503e73cf0e29ab55cd8ef1
jade570 <chaelyoung2naver.com>
2020-04-19 01:10:02 +0900

    [Update] label function & comment of how to add style in CSS

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
-let word1;
+let word1, word2;
+let div;
 
 //example
 function setup() {
 
-word = label("hello", "right", "blue", 18, 10, 10);
+word1 = label("hello world", "hello", "right", "blue", 18, 10, 10);
+word2 = label("what am I", "I", "center", "red", 25, 50, 10);
 
 //adding objects(DOM) to the textblock
 slider = createSlider(0, 100);
 slider.position(0, 20); //how much further from the label
-slider.parent(word); //type the label's variable which slider is related to
+slider.parent(word1); //type the label's variable which slider is related to
 }
--- text.js
+++ text.js
@@ -1,10 +1,9 @@
 ///label function///
-function label(text, align, color, size, x, y){
+function label(text, id, align, color, size, x, y){
   let div = createDiv(text);
-  div.id(text);
   div.position(x,y);
   div.style ('textAlign',align);
   div.style('color', color);
   div.style('font-size', size+'px');
-  return div.id(text);
+  return div.id(id);
 }
